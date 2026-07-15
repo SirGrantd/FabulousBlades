b499b07129971b8b27381052f3b4709a4b8736a4
G-Arantd <grantd.ctt@gmail.com>
2026-07-14 21:59:42 -0300

    Update

--- src/main/java/net/sirgrantd/fabulous_blades/init/FabulousBladesItems.java
+++ src/main/java/net/sirgrantd/fabulous_blades/init/FabulousBladesItems.java
@@ -22,20 +22,35 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.minecraft.world.item.Item;
 
 public class FabulousBladesItems {
-    public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(FabulousBladesMod.MODID);
-    public static final DeferredItem<Item> IRON_SWORD_CLAYMORE = REGISTRY.register("iron_sword_claymore", IronSwordClaymoreItem::new);
-	public static final DeferredItem<Item> IRON_SWORD_LONGBLADE = REGISTRY.register("iron_sword_longblade", IronSwordLongbladeItem::new);
-	public static final DeferredItem<Item> IRON_SWORD_KATANA = REGISTRY.register("iron_sword_katana", IronSwordKatanaItem::new);
-	public static final DeferredItem<Item> GOLD_SWORD_CLAYMORE = REGISTRY.register("gold_sword_claymore", GoldSwordClaymoreItem::new);
-	public static final DeferredItem<Item> GOLD_SWORD_LONGBLADE = REGISTRY.register("gold_sword_longblade", GoldSwordLongbladeItem::new);
-	public static final DeferredItem<Item> GOLD_SWORD_KATANA = REGISTRY.register("gold_sword_katana", GoldSwordKatanaItem::new);
-	public static final DeferredItem<Item> DIAMOND_SWORD_CLAYMORE = REGISTRY.register("diamond_sword_claymore", DiamondSwordClaymoreItem::new);
-	public static final DeferredItem<Item> DIAMOND_SWORD_LONGBLADE = REGISTRY.register("diamond_sword_longblade", DiamondSwordLongbladeItem::new);
-	public static final DeferredItem<Item> DIAMOND_SWORD_KATANA = REGISTRY.register("diamond_sword_katana", DiamondSwordKatanaItem::new);
-	public static final DeferredItem<Item> NETHERITE_SWORD_CLAYMORE = REGISTRY.register("netherite_sword_claymore", NetheriteSwordClaymoreItem::new);
-	public static final DeferredItem<Item> NETHERITE_SWORD_LONGBLADE = REGISTRY.register("netherite_sword_longblade", NetheriteSwordLongbladeItem::new);
-	public static final DeferredItem<Item> NETHERITE_SWORD_KATANA = REGISTRY.register("netherite_sword_katana", NetheriteSwordKatanaItem::new);
-	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_CLAYMORE = REGISTRY.register("reforge_template_sword_claymore", ReforgeTemplateSwordClaymoreItem::new);
-	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_LONGBLADE = REGISTRY.register("reforge_template_sword_longblade", ReforgeTemplateSwordLongbladeItem::new);
-	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_KATANA = REGISTRY.register("reforge_template_sword_katana", ReforgeTemplateSwordKatanaItem::new);
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(FabulousBladesMod.MODID);
+	public static final DeferredItem<Item> IRON_SWORD_CLAYMORE = REGISTRY.register("iron_sword_claymore",
+			IronSwordClaymoreItem::new);
+	public static final DeferredItem<Item> IRON_SWORD_LONGBLADE = REGISTRY.register("iron_sword_longblade",
+			IronSwordLongbladeItem::new);
+	public static final DeferredItem<Item> IRON_SWORD_KATANA = REGISTRY.register("iron_sword_katana",
+			IronSwordKatanaItem::new);
+	public static final DeferredItem<Item> GOLD_SWORD_CLAYMORE = REGISTRY.register("gold_sword_claymore",
+			GoldSwordClaymoreItem::new);
+	public static final DeferredItem<Item> GOLD_SWORD_LONGBLADE = REGISTRY.register("gold_sword_longblade",
+			GoldSwordLongbladeItem::new);
+	public static final DeferredItem<Item> GOLD_SWORD_KATANA = REGISTRY.register("gold_sword_katana",
+			GoldSwordKatanaItem::new);
+	public static final DeferredItem<Item> DIAMOND_SWORD_CLAYMORE = REGISTRY.register("diamond_sword_claymore",
+			DiamondSwordClaymoreItem::new);
+	public static final DeferredItem<Item> DIAMOND_SWORD_LONGBLADE = REGISTRY.register("diamond_sword_longblade",
+			DiamondSwordLongbladeItem::new);
+	public static final DeferredItem<Item> DIAMOND_SWORD_KATANA = REGISTRY.register("diamond_sword_katana",
+			DiamondSwordKatanaItem::new);
+	public static final DeferredItem<Item> NETHERITE_SWORD_CLAYMORE = REGISTRY.register("netherite_sword_claymore",
+			NetheriteSwordClaymoreItem::new);
+	public static final DeferredItem<Item> NETHERITE_SWORD_LONGBLADE = REGISTRY.register("netherite_sword_longblade",
+			NetheriteSwordLongbladeItem::new);
+	public static final DeferredItem<Item> NETHERITE_SWORD_KATANA = REGISTRY.register("netherite_sword_katana",
+			NetheriteSwordKatanaItem::new);
+	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_CLAYMORE = REGISTRY
+			.register("reforge_template_sword_claymore", ReforgeTemplateSwordClaymoreItem::new);
+	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_LONGBLADE = REGISTRY
+			.register("reforge_template_sword_longblade", ReforgeTemplateSwordLongbladeItem::new);
+	public static final DeferredItem<Item> REFORGE_TEMPLATE_SWORD_KATANA = REGISTRY
+			.register("reforge_template_sword_katana", ReforgeTemplateSwordKatanaItem::new);
 }
--- src/main/java/net/sirgrantd/fabulous_blades/init/ItemGroups.java
+++ src/main/java/net/sirgrantd/fabulous_blades/init/ItemGroups.java
@@ -11,23 +11,27 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 public class ItemGroups {
-    public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FabulousBladesMod.MODID);
-	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> FABULOUS_BLADES = REGISTRY.register("fabulous_blades", 
-    () -> CreativeModeTab.builder().title(Component.translatable("item_group.fabulous_blades.group")).icon(() -> new ItemStack(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_CLAYMORE.get())).displayItems((parameters, tabData) -> {
-        tabData.accept(FabulousBladesItems.IRON_SWORD_CLAYMORE.get());
-        tabData.accept(FabulousBladesItems.IRON_SWORD_LONGBLADE.get());
-        tabData.accept(FabulousBladesItems.IRON_SWORD_KATANA.get());
-        tabData.accept(FabulousBladesItems.GOLD_SWORD_CLAYMORE.get());
-        tabData.accept(FabulousBladesItems.GOLD_SWORD_LONGBLADE.get());
-        tabData.accept(FabulousBladesItems.GOLD_SWORD_KATANA.get());
-        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_CLAYMORE.get());
-        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_LONGBLADE.get());
-        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_KATANA.get());
-        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_CLAYMORE.get());
-        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_LONGBLADE.get());
-        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_KATANA.get());
-        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_CLAYMORE.get());
-        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_LONGBLADE.get());
-        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_KATANA.get());
-    }).build());
+    public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister
+            .create(Registries.CREATIVE_MODE_TAB, FabulousBladesMod.MODID);
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> FABULOUS_BLADES = REGISTRY.register(
+            "fabulous_blades",
+            () -> CreativeModeTab.builder().title(Component.translatable("item_group.fabulous_blades.group"))
+                    .icon(() -> new ItemStack(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_CLAYMORE.get()))
+                    .displayItems((parameters, tabData) -> {
+                        tabData.accept(FabulousBladesItems.IRON_SWORD_CLAYMORE.get());
+                        tabData.accept(FabulousBladesItems.IRON_SWORD_LONGBLADE.get());
+                        tabData.accept(FabulousBladesItems.IRON_SWORD_KATANA.get());
+                        tabData.accept(FabulousBladesItems.GOLD_SWORD_CLAYMORE.get());
+                        tabData.accept(FabulousBladesItems.GOLD_SWORD_LONGBLADE.get());
+                        tabData.accept(FabulousBladesItems.GOLD_SWORD_KATANA.get());
+                        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_CLAYMORE.get());
+                        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_LONGBLADE.get());
+                        tabData.accept(FabulousBladesItems.DIAMOND_SWORD_KATANA.get());
+                        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_CLAYMORE.get());
+                        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_LONGBLADE.get());
+                        tabData.accept(FabulousBladesItems.NETHERITE_SWORD_KATANA.get());
+                        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_CLAYMORE.get());
+                        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_LONGBLADE.get());
+                        tabData.accept(FabulousBladesItems.REFORGE_TEMPLATE_SWORD_KATANA.get());
+                    }).build());
 }
